3c14ce082845c4c80d3cf3ea2dbb640c26a9dcc9
Shashank Reddy <shashank.godala@gmail.com>
2023-03-24 12:12:49 -0700

    third-commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 // Write your JS code here
-import {withRouter} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import './index.css'
 
@@ -12,8 +12,43 @@ const Header = props => {
   }
 
   return (
-    <nav>
-      <button onClick={onClickLogout}>logout</button>
+    <nav className="nav-header">
+      <div className="nav-content">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          alt="website logo"
+        />
+        <ul className="nav-menu">
+          <Link to="/" className="nav-link">
+            <li>Home</li>
+          </Link>
+          <Link to="/products" className="nav-link">
+            <li>Products</li>
+          </Link>
+          <Link to="/cart" className="nav-link">
+            <li>Cart</li>
+          </Link>
+        </ul>
+        <button
+          type="button"
+          className="logout-desktop-btn"
+          onClick={onClickLogout}
+        >
+          Logout
+        </button>
+        <button
+          type="button"
+          className="logout-mobile-btn"
+          onClick={onClickLogout}
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+            alt="logout icon"
+            className="logout-icon"
+          />
+        </button>
+      </div>
     </nav>
   )
 }
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -44,33 +44,96 @@ class LoginForm extends Component {
     }
   }
 
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-filed"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-filed"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
   render() {
     const {username, password, showSubmitError, errorMsg} = this.state
     const jwtToken = Cookies.get('jwt_token')
     if (jwtToken !== undefined) {
       return <Redirect to="/" />
     }
-    return (
-      <div>
-        <form onSubmit={this.submitForm}>
-          <label htmlFor="username">username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={this.onChangeUsername}
-          />
+    // return (
+    //   <div>
+    //     <form onSubmit={this.submitForm}>
+    //       <label htmlFor="username">username</label>
+    //       <input
+    //         type="text"
+    //         id="username"
+    //         value={username}
+    //         onChange={this.onChangeUsername}
+    //       />
 
-          <label htmlFor="password">password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={this.onChangePassword}
+    //       <label htmlFor="password">password</label>
+    //       <input
+    //         type="password"
+    //         id="password"
+    //         value={password}
+    //         onChange={this.onChangePassword}
+    //       />
+    //       <button type="submit">Login</button>
+    //     </form>
+    //     {showSubmitError && <p>{errorMsg}</p>}
+    //   </div>
+    // )
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+        <form className="form-container" onSubmit={this.submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
           />
-          <button type="submit">Login</button>
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          {showSubmitError && <p className="error-message">*{errorMsg}</p>}
         </form>
-        {showSubmitError && <p>{errorMsg}</p>}
       </div>
     )
   }
